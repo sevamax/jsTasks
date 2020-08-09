761fac5304646d05330510ac7f83abdc01be712d
sevamax <seva_max2000@ukr.net>
2020-08-09 15:31:35 +0300

    Practice 2 Task 4.1

--- js/main.js
+++ js/main.js
@@ -12,25 +12,25 @@ const personalMovieDB = {
   privat: false
 };
 
-let countMessage = (personalMovieDB.count >= 0 && personalMovieDB.count < 10) ? 'Просмотрено довольно мало фильмов' : 
-                   (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) ? 'Вы классический зритель':
-                   (personalMovieDB.count > 30) ? 'Вы киноман' :
-                   `Произошла ошибка. Перезагрузите страницу и попробуйте еще  ${personalMovieDB.count} `;
-console.log(countMessage);
+// let countMessage = (personalMovieDB.count >= 0 && personalMovieDB.count < 10) ? 'Просмотрено довольно мало фильмов' : 
+//                    (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) ? 'Вы классический зритель':
+//                    (personalMovieDB.count > 30) ? 'Вы киноман' :
+//                    `Произошла ошибка. Перезагрузите страницу и попробуйте еще  ${personalMovieDB.count} `;
+// console.log(countMessage);
 
+let i = 1;
+while(i < 3) {
+  let lastFilmFirst = prompt("Один из последних просмотренных фильмов");
+  let rangeFirst = +prompt("На сколько оцените его?");
 
-// for(let i = 1; i < 3; i++) {
-//   let lastFilmFirst = prompt("Один из последних просмотренных фильмов");
-//   let rangeFirst = +prompt("На сколько оцените его?");
+  if(  ( lastFilmFirst && rangeFirst != null ) && ( lastFilmFirst && rangeFirst != "" ) && ( lastFilmFirst.length <= 50 ) ) {
+    personalMovieDB.movies[lastFilmFirst] = rangeFirst;
 
-//   if(  ( lastFilmFirst && rangeFirst != null ) && ( lastFilmFirst && rangeFirst != "" ) && ( lastFilmFirst.length <= 50 ) ) {
-//     personalMovieDB.movies[lastFilmFirst] = rangeFirst;
-
-//   } else {    
-//     console.log(`${lastFilmFirst} - Название фильма не может быть больше, чем 50 строк или быть пустой строкой/n Оценка не может быть пустой`);
-//     alert(`${lastFilmFirst} - Название фильма не может быть больше, чем 50 строк или быть пустой строкой/n Оценка не может быть пустой`);
-//     --i;
-//   }
-
-// }
-// console.log(personalMovieDB);
+  } else {    
+    console.log(`${lastFilmFirst} - Название фильма не может быть больше, чем 50 строк или быть пустой строкой/n Оценка не может быть пустой`);
+    alert(`${lastFilmFirst} - Название фильма не может быть больше, чем 50 строк или быть пустой строкой/n Оценка не может быть пустой`);
+    --i;
+  }
+  i++;
+}
+console.log(personalMovieDB);
