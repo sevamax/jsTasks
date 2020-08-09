be6855f83928172b797ea20514b699229aa85c5f
sevamax <seva_max2000@ukr.net>
2020-08-09 14:07:17 +0300

    Practice 1 Task 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фиьмов вы уже посмотрели?");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+
+  },
+  actors: {
+    
+  },
+  genres: [],
+  privat: false
+};
